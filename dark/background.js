a3b7a92e7b076b50d410f680e713cef247429bb4
laleyou <osw.kesnco@gmail.com>
2025-10-15 22:12:31 -0400

    update to manifest 3 test 1

--- dark/background.js
+++ dark/background.js
@@ -1,8 +1,8 @@
 let state = false;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(tab => {
   if (!state) {
-    chrome.tabs.onUpdated.addListener() {
+    chrome.scripting.onUpdated.addListener() {
 	for (var i=0; i<document.getElementsByTagName("*").length; i++) document.getElementsByTagName("*")[i] = document.getElementsByTagName("*")[i].setAttribute("style","background-color:black; color:red");
     }
     state = !state;
