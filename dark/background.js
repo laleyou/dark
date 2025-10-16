c696cc1cd5b2b42ff4bcdaf7d0b43684b87d8f97
laleyou <osw.kesnco@gmail.com>
2025-10-16 00:52:24 -0400

    fixed bug from misnterpretation of ternary operator as value on single line

--- dark/background.js
+++ dark/background.js
@@ -1,4 +1,5 @@
 for (var i=0; i<document.getElementsByTagName("*").length; i++) {
   var element = document.getElementsByTagName("*")[i] 
-  element = element.setAttribute("style", element.getAttribute("style")===null?"":element.getAttribute("style") + "; background-color:black; color:red");
+  var css = "background-color:black; color:red"
+  element = element.setAttribute("style", element.getAttribute("style")===null ? css : element.getAttribute("style")+"; "+css);
 }
