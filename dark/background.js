6e986d45de54305ccb0a91f904cd636693a72d91
laleyou <osw.kesnco@gmail.com>
2025-10-15 22:33:48 -0400

    simplify

--- dark/background.js
+++ dark/background.js
@@ -1,11 +1,3 @@
-let state = false;
-
-chrome.action.onClicked.addListener(tab => {
-  if (!state) {
-    chrome.scripting.onUpdated.addListener() {
-	for (var i=0; i<document.getElementsByTagName("*").length; i++) document.getElementsByTagName("*")[i] = document.getElementsByTagName("*")[i].setAttribute("style","background-color:black; color:red");
-    }
-    state = !state;
-    return;
-  }
-});
+chrome.scripting.onUpdated.addListener() {
+    for (var i=0; i<document.getElementsByTagName("*").length; i++) document.getElementsByTagName("*")[i] = document.getElementsByTagName("*")[i].setAttribute("style","background-color:black; color:red");
+}
